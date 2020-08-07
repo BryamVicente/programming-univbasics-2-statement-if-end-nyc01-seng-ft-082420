29fdb306c52a3d7b660bd9b0d091e4b5ac17c81e
Bryam Vicente <vicentebryam@gmail.com>
2020-08-07 20:14:51 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -13,8 +13,10 @@ if run_code_inside
 end
   puts "Code after if...end"
 
-current_time = time.now
-if current_time == even number
+current_time = Time.now
+current_time = current_time.to_i
+
+if current_time.even?
   puts "Even!"
 else
   puts "Odd!"
