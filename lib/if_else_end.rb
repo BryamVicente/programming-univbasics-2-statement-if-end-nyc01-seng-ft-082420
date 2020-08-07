f087a7239ecf299fc0f147de8ae3dbf045ce01d8
Bryam Vicente <vicentebryam@gmail.com>
2020-08-07 19:58:34 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,3 +5,10 @@ if run_code_inside
   puts "code inside"
 end
   puts "Code after if...end"
+
+
+run_code_inside = false
+puts "code before if...end"
+if run_code_inside
+end
+  puts "Code after if...end"
